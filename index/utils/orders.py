50d568f76379645a9faab2e257fe9e6397b04ab5
Yerassyliitu <34080@iitu.edu.kz>
2024-02-10 16:55:56 +0600

    gunicorn timeout error 1.2

--- index/utils/orders.py
+++ index/utils/orders.py
@@ -3,24 +3,26 @@ from datetime import datetime
 from index.utils.connect_firebase import db, order_collection_path, to_receiving_country, history_status_collection_path
 
 
-def add_order(data):
+async def add_order(data):
     track_id = data["track_id"]
-    if check_if_order_exists(track_id):
-        order_id = get_order_id_by_track_id(track_id)
-        return replace_order_status(order_id, to_receiving_country)
+    if await check_if_order_exists(track_id):
+        order_id = await get_order_id_by_track_id(track_id)
+        return await replace_order_status(order_id, to_receiving_country)
     else:
-        doc_ref = db.collection(order_collection_path).add(data)
-        return doc_ref[1].id if doc_ref[1].id else False
+        doc_ref = await db.collection(order_collection_path).add(data)
+        return doc_ref.id if doc_ref.id else False
 
 
-def check_if_order_exists(track_id):
-    docs = db.collection(order_collection_path).where("track_id", "==", track_id).stream()
-    return len(list(docs)) > 0
+async def check_if_order_exists(track_id):
+    query = db.collection(order_collection_path).where("track_id", "==", track_id).limit(1)
+    async for _ in query.stream():
+        return True
+    return False
 
 
-def get_order_id_by_track_id(track_id):
-    docs = db.collection(order_collection_path).where("track_id", "==", track_id).stream()
-    for doc in docs:
+async def get_order_id_by_track_id(track_id):
+    query = db.collection(order_collection_path).where("track_id", "==", track_id).limit(1)
+    async for doc in query.stream():
         return doc.id
     return None
 
@@ -28,17 +30,15 @@ def get_order_id_by_track_id(track_id):
 async def replace_order_status(order_id, new_status_reference):
     order_ref = db.collection(order_collection_path).document(order_id)
 
-    # Получаем текущий статус заказа
     order_snapshot = await order_ref.get()
     order_data = order_snapshot.to_dict()
     old_status_reference = order_data.get('status')
 
     if old_status_reference == new_status_reference:
         return f"Order {order_id} already has status {new_status_reference.id}"
-    # Обновляем статус заказа
+
     await order_ref.update({"status": new_status_reference})
 
-    # Добавляем запись в историю изменений статуса заказа
     history_data = {
         "old_status": old_status_reference,
         "new_status": new_status_reference,
