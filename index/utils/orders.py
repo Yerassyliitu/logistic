1a1fd4b058eefd7d4cc86ab32531cec11c4e9c87
Yerassyliitu <34080@iitu.edu.kz>
2024-03-08 00:49:20 +0500

    deploy on server 9

--- index/utils/orders.py
+++ index/utils/orders.py
@@ -2,30 +2,31 @@ from index.utils.connect_firebase import db
 
 
 def change_order_status(rows, status_admin, field_to_parse):
-    user_docs = db.collection("users").stream()
+    # Загрузить все документы из коллекции "users_op"
+    users_op_ref = db.collection_group("users_op").stream()
+    user_op_data_dict = {}
+
+    # Собираем данные в локальную структуру данных
+    for user_op_doc in users_op_ref:
+        user_op_data = user_op_doc.to_dict()
+        barcode = user_op_data.get("barcode")
+        if barcode:
+            user_op_data_dict[barcode] = {"user_doc_id": user_op_doc.reference.parent.parent.id, "user_op_doc_ref": user_op_doc.reference}
+
+    # Обрабатываем данные из rows
     array = []
     for row in rows:
         track_id = row[field_to_parse - 1]
-        if track_id == None or track_id == '' or track_id == 'None':
-            continue
-        try:
-            track_id = int(track_id)
-        except:
-            track_id = track_id
-        # print(track_id)
-        array.append(track_id)
-    count = 0
-    for user_doc in user_docs:
-        users_op_ref = db.collection("users").document(user_doc.id).collection("users_op").stream()
-        for user_op_doc in users_op_ref:
-            user_op_data = user_op_doc.to_dict()
-            for track_id in array:
-                if track_id == user_op_data["barcode"]:
-                    print(track_id)
-                    user_op_doc.reference.set({"status_admin": status_admin}, merge=True)
-                    array.remove(track_id)
-                    break
+        if track_id and track_id != 'None':
+            array.append(track_id)
+    filtered_array = []
+    # Обновляем статусы в базе данных Firebase и в локальной структуре данных
+    for track_id in array:
+        if track_id in user_op_data_dict:
+            user_op_data_dict[track_id]["user_op_doc_ref"].set({"status_admin": status_admin}, merge=True)
+            del user_op_data_dict[track_id]  # Удаляем обработанный элемент из локальной структуры данных
+        else:
+            filtered_array.append(track_id)
 
-    return array
-
+    return filtered_array
 
