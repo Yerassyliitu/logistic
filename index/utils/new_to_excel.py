d6fd746082f82fc067df87097da0a8b6236a6ad1
Yerassyliitu <34080@iitu.edu.kz>
2024-03-08 12:59:29 +0500

    deploy on server 10

--- index/utils/new_to_excel.py
+++ index/utils/new_to_excel.py
@@ -3,12 +3,16 @@ from index.utils.orders import change_order_status
 
 
 def parse_from_excel_function(filename, status_admin, field_to_parse):
+    all_columns = []
     try:
         wb = openpyxl.load_workbook(filename)
         sheet = wb.active
-        all_unchanged_track_ids = change_order_status(sheet.iter_rows(min_row=0, values_only=True), status_admin, field_to_parse)
-
-        return all_unchanged_track_ids
+        for row in sheet.iter_rows(min_row=0, values_only=True):
+            column_1_data = row[field_to_parse - 1]
+            if column_1_data != None:
+                all_columns.append(column_1_data)
+        unchanged = change_order_status(all_columns, status_admin)
+        return unchanged
     except Exception as e:
         print(e)
 
--- index/utils/orders.py
+++ index/utils/orders.py
@@ -1,7 +1,7 @@
 from index.utils.connect_firebase import db
 
 
-def change_order_status(rows, status_admin, field_to_parse):
+def change_order_status(array, status_admin):
     # Загрузить все документы из коллекции "users_op"
     users_op_ref = db.collection_group("users_op").stream()
     user_op_data_dict = {}
@@ -12,21 +12,16 @@ def change_order_status(rows, status_admin, field_to_parse):
         barcode = user_op_data.get("barcode")
         if barcode:
             user_op_data_dict[barcode] = {"user_doc_id": user_op_doc.reference.parent.parent.id, "user_op_doc_ref": user_op_doc.reference}
-
-    # Обрабатываем данные из rows
-    array = []
-    for row in rows:
-        track_id = row[field_to_parse - 1]
-        if track_id and track_id != 'None':
-            array.append(track_id)
     filtered_array = []
     # Обновляем статусы в базе данных Firebase и в локальной структуре данных
     for track_id in array:
+        track_id = str(track_id)
         if track_id in user_op_data_dict:
             user_op_data_dict[track_id]["user_op_doc_ref"].set({"status_admin": status_admin}, merge=True)
-            del user_op_data_dict[track_id]  # Удаляем обработанный элемент из локальной структуры данных
+            print(track_id)
+            del user_op_data_dict[track_id]
         else:
             filtered_array.append(track_id)
-
+    print(filtered_array)
     return filtered_array
 
